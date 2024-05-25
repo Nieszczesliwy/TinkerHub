d34d3d81b7a5463f05c00c5a55cbac886cb6af44
Jorge Zborivskyi <zborivskyi@Jorges-MacBook-Air.local>
2024-05-25 06:57:51 +0300

    Continue testing articles

--- articles/models.py
+++ articles/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class Article(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    author = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, default='Default title')
+    description = models.CharField(max_length=400, default='Default description')
+    content = models.TextField(default='Default content')
+    author = models.CharField(max_length=100, default='Terry Davis')
     published_date = models.DateTimeField(auto_now_add=True)
-    
 
     def __str__(self):
         return self.title
